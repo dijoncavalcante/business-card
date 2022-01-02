55b4841318decaf249fb981cdaa99c6ce68d0c36
Dijon Cavalcante Braga <dijon.braga@btgpactual.com>
2022-01-02 18:06:29 -0300

    feat: Add configuration room database part 3

--- app/src/main/java/com/dijon/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/dijon/businesscard/ui/AddBusinessCardActivity.kt
@@ -1,12 +1,21 @@
 package com.dijon.businesscard.ui
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.dijon.businesscard.App
+import com.dijon.businesscard.R
+import com.dijon.businesscard.data.BusinessCard
 import com.dijon.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,15 @@ class AddBusinessCardActivity : AppCompatActivity() {
                 finish()
             }
             btnConfirm.setOnClickListener {
+                val businessCard = BusinessCard(
+                    name = tilName.editText?.text.toString(),
+                    company = tilCompany.editText?.text.toString(),
+                    cellphone = tilCellphone.editText?.text.toString(),
+                    email = tilEmail.editText?.text.toString(),
+                    backgroundCustom = tilColor.editText?.text.toString(),
+                )
+                mainViewModel.insert(businessCard)
+                Toast.makeText(this@AddBusinessCardActivity, R.string.label_show_success, Toast.LENGTH_SHORT).show()
                 finish()
             }
         }
